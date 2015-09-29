5531a8934d2abe42c5d470e513769a1eb0359483
DanielHeo94 <peterjinsoo@gmail.com>
2015-09-29 21:39:33 +0900

    ddd

--- readMS5611.c
+++ readMS5611.c
@@ -1,10 +1,16 @@
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/types.h>
 #include <fcntl.h>
-#include <string.h>
-
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
+#include <math.h>
 
 #define MS5611_ADDRESS 0x77
 
@@ -28,42 +34,67 @@
 #define OSR_2048     5
 #define OSR_4096     10
 
+
+
 unsigned int PROM_read(int DA, int PROM_CMD)
 {
-	unsigned int PROM_DATA;
+	uint16_t ret = 0;
+	uint8_t r8b[] = { 0, 0 };
 
-	wiringPiI2CWrite(DA, PROM_CMD);
-	read(DA, PROM_DATA, 2);
+	if (write(DA, PROM_CMD, 1) != 1){
+		printf("read set reg Failed to write to the i2c bus.\n");
+	}
 
-	return PROM_DATA;
+	if (read(fd, r8b, 2) != 2){
+		printf("Failed to read from the i2c bus.\n");
+	}
+
+	ret = (uint16_t(r8b[0]) << 8) | uint16_t(r8b[1]);
+	return ret;
 }
-
-/*unsigned int BARO_read(int DA, int BARO_CMD)
-{
-	return i2c_smbus_read_block_data(DA, BARO_CMD);
-}*/
 
 void main()
 {
-	int Device_Address;
-	int i;
+	unsigned int i;
 
-	unsigned int C[6];
+	int fd;
+
+	unsigned int C[7];
+
 	unsigned long D1;
 	unsigned long D2;
 
-	Device_Address = wiringPiI2CSetup(MS5611_ADDRESS);
+	float P;
+	float T;
+	float dT;
+	float OFF;
+	float SENS;
+	float H_alt;
+	float H_temp;
+	float Altitude;
+	char buf0[26] = { 0, };
 
-	for (i = 0; i < 6; i++)
-	{
-		C[i] = PROM_read(Device_Address, CMD_PROM_READ + i * 2);
-
-		printf("C[%d] = %d\n", i + 1, C[i]);
+	if ((fd = open("/dev/i2c-1", O_RDWR)) < 0){
+		printf("Failed to open the bus.\n")
+			return -1;
 	}
 
-	//D1 = BARO_read(Device_Address, CONV_D1_4096);
-	//D2 = BARO_read(Device_Address, CONV_D2_4096);
+	if (ioctl(fd, I2C_SLAVE, MS5611_ADDRESS) < 0){
+		printf("Failed to acquire bus access and/or talk to slave.\n")
+			return -1;
+	}
 
-	//printf("D1 = %d\n", D1);
-	//printf("D2 = %d", D2);
+	if (write(fd, RESET, 1) != 1) {
+		printf("write reg 8 bit Failed to write to the i2c bus.\n");
+	}
+
+	usleep(10000);
+
+	for (i = 0; i < 7; i++){
+		usleep(1000);
+
+		C[i] = PROM_read(fd, CMD_PROM_READ + (i * 2));
+		printf("C[%d] = %d\n", i+1, C[i]);
+	}
+
 }
