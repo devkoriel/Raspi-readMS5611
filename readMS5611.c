055224eb80d0780dcd48846df1e749f78e63bfda
DanielHeo94 <peterjinsoo@gmail.com>
2015-09-30 02:57:12 +0900

    tx_buffer

--- readMS5611.c
+++ readMS5611.c
@@ -14,6 +14,9 @@
 #include <math.h>
 #include <termios.h> // Used for UART
 
+#include <wiringPi.h>
+#include <wiringPiSerial.h>
+
 #define MS5611_ADDRESS 0x77
 
 #define CONV_D1_256   0x40
@@ -87,53 +90,6 @@ long CONV_read(int DA, char CONV_CMD)
 	return ret;
 }
 
-void setup_USART()
-{
-	//-------------------------
-	//----- SETUP USART 0 -----
-	//-------------------------
-	//At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
-	int uart0_filestream = -1;
-
-	//OPEN THE UART
-	//The flags (defined in fcntl.h):
-	//	Access modes (use 1 of these):
-	//		O_RDONLY - Open for reading only.
-	//		O_RDWR - Open for reading and writing.
-	//		O_WRONLY - Open for writing only.
-	//
-	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
-	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
-	//											immediately with a failure status if the output can't be written immediately.
-	//
-	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
-	uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
-	if (uart0_filestream == -1)
-	{
-		//ERROR - CAN'T OPEN SERIAL PORT
-		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
-	}
-
-	//CONFIGURE THE UART
-	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
-	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
-	//	CSIZE:- CS5, CS6, CS7, CS8
-	//	CLOCAL - Ignore modem status lines
-	//	CREAD - Enable receiver
-	//	IGNPAR = Ignore characters with parity errors
-	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
-	//	PARENB - Parity enable
-	//	PARODD - Odd parity (else even)
-	struct termios options;
-	tcgetattr(uart0_filestream, &options);
-	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
-	options.c_iflag = IGNPAR;
-	options.c_oflag = 0;
-	options.c_lflag = 0;
-	tcflush(uart0_filestream, TCIFLUSH);
-	tcsetattr(uart0_filestream, TCSANOW, &options);
-}
-
 void main()
 {
 	int i;
@@ -158,7 +114,7 @@ void main()
 
 	float Altitude;
 
-	char buf0[26] = { 0, };
+	unsigned char tx_buffer[4];
 
 	if ((fd = open("/dev/i2c-1", O_RDWR)) < 0){
 		printf("Failed to open the bus.\n");
@@ -182,8 +138,6 @@ void main()
 		C[i] = PROM_read(fd, CMD_PROM_READ + (i * 2));
 		//printf("C[%d] = %d\n", i, C[i]);
 	}
-
-	setup_USART();
 
 	while (1){
 		D1 = CONV_read(fd, CONV_D1_4096);
@@ -230,7 +184,10 @@ void main()
 
 		Altitude = ((pow((SEA_LEVEL_PRESSURE / Pressure), 1 / 5.257) - 1.0) * (Temparature + 273.15)) / 0.0065;
 
-		printf("  Altitude : %.2f m\n", Altitude);
+		printf("  Altitude : %.2f m", Altitude);
+
+		tx_buffer = (char)(Altitude * 100);
+		printf("  tx_buffer : %d\n", tx_buffer);
 
 	}
 }
